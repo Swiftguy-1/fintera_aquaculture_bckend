16e03d57ae4516304dbf61503f09491031d7e180
Bright Kelechi <brightkelechi009@gmail.com>
2026-09-03 02:57:22 +0100

    Update main.py
    Fixed minor issue

--- main.py
+++ main.py
@@ -27,12 +27,14 @@ from user_mgt import router as mgt_router
 
 app = FastAPI(title="User Dashboard Security System")
 
+ALLOWED_ORIGINS=[fintera-app.vercel.app]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type", "Accept"],
 )
 
 app.include_router(mortality_router)
