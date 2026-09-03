1a52103f83c45199274005dcd46b66ec55f8fb88
Bright Kelechi <brightkelechi009@gmail.com>
2026-09-03 11:49:47 +0100

    Update patch.py

--- patch.py
+++ patch.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
-from auth import require_admin
+from dependencies import require_admin
 from database import supabase
 
 router = APIRouter(prefix="/admin", tags=["Admin Dashboard"])
